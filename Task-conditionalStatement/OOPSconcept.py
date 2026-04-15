a7c5728c699b7b66794cba0228f0284ef735854d
samuel <samuelsam8322@gmail.com>
2026-04-15 11:59:26 +0530

    python

--- Task-conditionalStatement/OOPSconcept.py
+++ Task-conditionalStatement/OOPSconcept.py
@@ -68,3 +68,260 @@ print(dog1.species)
 #        * Encapsulation
 #        * Polymorphism
 #        * Abstraction  
+
+
+# 1. Inheritance:
+#       The Mechanism by which one class(called the child or derived class) can access the atributes 
+# and methods of another class(called parent or base class.)
+
+# why use Inheritance:
+#       * code reusability - avoid duplicating
+#       * organized structure - logical grouping of related classes.
+#       * Extensibility - easy to add new feature extending existing classes.
+
+#   Types:
+
+#           * Single Inheritance
+#           * Multiple Inheritance
+#           * Multilevel Inheritance
+#           * Hierarical Inheritance
+#           * Hybrid Inheritance
+
+# i. Single Inheritance:
+#       A child class inherits from on parent class
+
+class Parent:
+    def show(self):
+        print("Parent class")
+
+class Child(Parent):
+    pass
+
+c = Child()
+c.show()
+
+
+# ii.Multiple Inheritance:
+#       One child class inherits from multiple parent classes.
+class A:
+    def method_a(self):
+        print("Class A")
+
+class B:
+    def method_b(self):
+        print("Class B")
+
+class C(A, B):
+    pass
+
+obj = C()
+obj.method_a()
+obj.method_b()
+
+# iii. Multilevel Inheritance:
+#       Inheritance in levels (chain).
+class Grandparent:
+    def show1(self):
+        print("Grandparent")
+
+class Parent(Grandparent):
+    def show2(self):
+        print("Parent")
+
+class Child(Parent):
+    def show3(self):
+        print("Child")
+
+c = Child()
+c.show1()
+c.show2()
+c.show3()
+
+
+# iv.Multilevel Inheritance:
+#       Inheritance in levels (chain).
+class Grandparent:
+    def show1(self):
+        print("Grandparent")
+
+class Parent(Grandparent):
+    def show2(self):
+        print("Parent")
+
+class Child(Parent):
+    def show3(self):
+        print("Child")
+
+c = Child()
+c.show1()
+c.show2()
+c.show3()
+
+# v. Hybrid Inheritane:
+#       Combination of two or more types of inheritance (combination of multiple and multilevel 
+#  Inheritance).
+class A:
+    def show(self):
+        print("Class A")
+
+class B(A):
+    pass
+
+class C(A):
+    pass
+
+class D(B, C):
+    pass
+
+d = D()
+d.show()
+
+
+# 2. POLYMORPHISM:
+#           Polymorphism means ' many forms'- same method, function, or operators can behave 
+# differently depending on the object or data it is used with.
+
+# Types:
+
+# i.Compile-Time Polymorphism (Method Overloading) or static typing.
+#           Python does not support traditional overloading, but we achieve it using:
+#       * default arguments
+#       * variable-length arguments (*args)
+class Math:
+    def add(self, a, b=0, c=0):
+        return a + b + c
+
+m = Math()
+
+print(m.add(2, 3))       
+print(m.add(2, 3, 4))    
+
+# ii.Method Overriding (Runtime Polymorphism)
+#           When a child class provides its own implementation of a method already defined in 
+# the parent class.
+class Animal:
+    def sound(self):
+        print("Animal makes sound")
+
+class Dog(Animal):
+    def sound(self):   # overriding
+        print("Dog barks")
+
+class Cat(Animal):
+    def sound(self):   # overriding
+        print("Cat meows")
+
+# usage
+a = Animal()
+d = Dog()
+c = Cat()
+
+a.sound()
+d.sound()
+c.sound()
+
+# iii.Operator Overloading (Special Method)
+#           Using magic methods like __add__, __str__, etc.
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    def __add__(self, other):
+        return Number(self.value + other.value)
+
+    def __str__(self):
+        return str(self.value)
+
+n1 = Number(10)
+n2 = Number(20)
+
+print(n1 + n2)
+
+#       Type	                             Description
+# Method Overriding	        -    Same method in parent & child class
+# Method Overloading	        -    Same method, different parameters
+# Operator Overloading	    -    Custom behavior for operators
+
+# 3.ENCAPSULATION:
+#       Encapsulation means wrapping or binding data(variables) and methods(functions) together
+# inside a class and restricting direct access to some data.
+#       It helps protects data from accidental modification.
+
+# Access Modifiers:
+#       In python define the visibility of class members using naming conventions like
+# public, protected, and private.
+
+#   Modifier	 |       Symbol	     |       Access Level
+
+# Public	             name	            Anywhere
+# Protected	        _name	            Class + Subclass
+# Private	           __name	            Only inside class
+
+# i.Public Encapsulation:
+#   Public variables and methods can be accessed from anywhere(inside or outside).
+
+class Student():
+    def __init__ (self,name):
+        self.name = name
+
+    def display(self):
+        print("Student Name:",self.name)
+
+s = Student("Sam")
+#print(s.name)
+s.display()
+
+# ii. Protected Encapsulation:
+#       protected members members are indicated with single _underscore they should not be 
+# accessed outside the class, but python still allows it (by convention).
+
+class Student():
+    def __init__ (self,name):
+        self._name = name
+    
+class Child(Student):
+    def show(self):
+        print("Name:",self._name)
+
+c = Child("Strange")
+c.show()
+
+
+# iii. Private Encapsulation:
+#       Private members are indicated with double __underscore.
+#       They can't be accessed directly outside the class.
+
+class Bank():
+    def __init__(self,balance):
+        self.__balance = balance
+
+    def show_balance(self):
+        print("Balance:",self.__balance)
+
+b = Bank(10000)
+b.show_balance()
+
+ # iv. Getter and Setter methods:
+ #      Getter - used to read/ access private data.
+ #      Setter - used to update/modify private data safely.
+
+class Employee:
+    def __init__(self,salary):
+        self.__salary = salary
+
+    def get_salary(self):           # getter method 
+        return self.__salary
+    
+    def set_salary(self,new_salary):        # setter method
+        if new_salary > 0:
+            self.__salary = new_salary
+        else:
+            print("Invalid Salary")
+
+emp = Employee(50000)
+print("Salary:",emp.get_salary())       # getter
+
+emp.set_salary(1400000)
+print("Updated Salary:",emp.get_salary())       # setter
+
+        
